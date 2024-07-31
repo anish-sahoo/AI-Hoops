851ee4ab4eed3d72f35b3c31aacde40bc0384133
Ethan Carpenter <ethanCarpenterMail@gmail.com>
2024-07-31 12:52:00 -0400

    added read

--- training.py
+++ training.py
@@ -3,7 +3,12 @@ import torch
 import keras
 import numpy as np
 import pandas as pd
+
 import json
+import zipfile as zf
+import io
+import os
+
 
 import gymnasium as gym
 from ale_py import ALEInterface
@@ -18,11 +23,49 @@ ale.loadROM(DoubleDunk)
 
 env = gym.make('ALE/DoubleDunk-v5', render_mode='human', obs_type="ram")
 
-f = open("meta_data.txt", "r")
-size = json.loads(f.read())["ram_size"]
-f.close()
+def generate_model():
+    # define the keras model
+    model = Sequential()
+    model.add(Dense(env.observation_space.shape[0]*2, input_shape=env.observation_space.shape, activation='relu'))
+    #model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
+def generate_data():
+    dir = "data"
+    max_i = 0
+    while os.path.isfile(dir + str(max_i) + "_compressed.zip"):
+        max_i += 1
 
+    for i in range(max_i):
+        dir = "data" + str(i)
+        zip_name = dir + "_compressed.zip"
+
+        with zf.ZipFile(zip_name) as f:
+            file_names = f.namelist()
+            for file_name in file_names:
+                with io.TextIOWrapper(f.open(file_name), encoding="utf-8") as data_file:
+                    yield json.loads(str(data_file.read()))
+
+def train_model(model):
+    for data in generate_data():
+        print(len(data))
+        for data_set in data:
+            if(np.array(data_set[0][0]).shape != 128):
+                print(np.array(data_set[0][0]).shape)
+            if(np.array(data_set[1]).shape != 1):
+                print(np.array(data_set[1]).shape)
+            model.fit(np.array(data_set[0][0]), data_set[1])
+
+def main():
+    model = generate_model()
+    train_model(model)
+
+if __name__ == "__main__":
+    main()
+
+'''
 # define the keras model
 model = Sequential()
 model.add(Dense(env.observation_space.shape[0]*2, input_shape=env.observation_space.shape, activation='relu'))
@@ -38,3 +81,4 @@ model.fit([x[0] for x in data], [x[1] for x in data])
 _, accuracy = model.evaluate([x[0] for x in data], [x[1] for x in data], verbose=0)
 print("accuracy: ", accuracy)
 env.close()
+'''
