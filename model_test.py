a7411cd1fca5fabe2d31af75602aba1c1226aaab
Anish Sahoo <anishsahoo2005@gmail.com>
2024-08-06 23:24:25 -0400

    Finally working output

--- model_test.py
+++ model_test.py
@@ -3,7 +3,7 @@ import gymnasium as gym
 from ale_py import ALEInterface
 from ale_py.roms import DoubleDunk
 import matplotlib.pyplot as plt
-from doubledunk_training import DeepQNetwork
+from old_files.doubledunk_training import DeepQNetwork
 import torch
 
 ale = ALEInterface()
@@ -18,17 +18,14 @@ policy_net.eval()
 next_state, _ = env.reset()
 done = False
 
-def get_action_probabilities(policy_net, state):
-    policy_net.eval()
-    with torch.no_grad():
-        state = torch.FloatTensor(state).unsqueeze(0)
-        return policy_net(state).squeeze()
 
 moves = {1:"NOOP", 2:"FIRE", 3:"UP", 4:"RIGHT", 5:"LEFT", 6:"DOWN", 7:"UPRIGHT", 8:"UPLEFT", 9:"DOWNRIGHT", 10:"DOWNLEFT", 11:"UPFIRE", 12:"RIGHTFIRE", 13:"LEFTFIRE", 14:"DOWNFIRE", 15:"UPRIGHTFIRE", 16:"UPLEFTFIRE", 17:"DOWNRIGHTFIRE", 18:"DOWNLEFTFIRE"}
 next_state, reward, done, truncated, info = env.step(1)
 while not done:
-    action_probabilities = get_action_probabilities(policy_net, next_state)
+    policy_net.eval()
+    with torch.no_grad():
+        action_probabilities = policy_net(torch.tensor(next_state, dtype=torch.float32).unsqueeze(0))
     next_state, reward, done, truncated, info = env.step(action_probabilities.argmax())
     env.render()
-    
+input()
 env.close()
