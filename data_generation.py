33393b2140a28360a27b776b226b31f4ba9f65d5
sanaali03 <113324641+sanaali03@users.noreply.github.com>
2024-08-12 19:58:43 -0400

    Updated data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -1,19 +1,22 @@
-# could use python 3.11
+# imports - works with python 3.11 & 3.12
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+# utilities
 import multiprocessing as mp
 import json
 import shutil as sl
 import os
 
+# handling the game environment
 import gymnasium as gym
 from ale_py import ALEInterface
 from ale_py.roms import DoubleDunk
 
+# initializing the Atari Learning Environment
 ale = ALEInterface()
-ale.loadROM(DoubleDunk)
+ale.loadROM(DoubleDunk)  # loading Double Dunk
 
 
 # no rendering : cpu does the work, faster but massive load on cpu (not recommended)
@@ -22,6 +25,18 @@ env = gym.make('ALE/DoubleDunk-ram-v5', obs_type="ram", render_mode="human")
 
 
 def play(pair):
+    """
+    
+    This function instantiates the Double Dunk
+    game environment for the agent to play. During
+    execution, each action, state and reward is
+    recorded to a JSON file.
+
+    params: 
+        pair(tuple): contains the string directory 
+        to store results and identifier number.
+        
+    """
     dir, num = pair
     obs, _ = env.reset()
     obs = np.array(obs)
@@ -53,11 +68,25 @@ def play(pair):
     env.close()
 
 def run(num_instances, num_total, dir):
+    """
+    
+    This function executes the 'play' function
+    and compresses the resulting data 
+    directory into a zip file.
+
+    params: 
+        num_instances (int): number of instances to run
+        the game.
+        num_total (int) : the number of simulation runs.
+        dir(string): directory to save the results.
+        
+    """
     with mp.Pool(num_instances) as p:
         p.map(play, zip([dir]*num_total, range(num_total)))
     sl.make_archive(dir+"_compressed", "zip", dir)
     sl.rmtree(dir)
 
+# main function
 if __name__ == "__main__":
     num_instances = 50 # update this
     runs_per_batch = 1000 # update this
